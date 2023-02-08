1e05fa392b3034bb9f576afa06638fe2926ebc21
MARC_DIONIS_DUMANGAS <marcdionis.dumangas.20@usjr.edu.ph>
2023-02-08 14:45:18 +0000

    Added CurvedNavigationBar

--- lib/pages/Homepage.dart
+++ lib/pages/Homepage.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import '../widgets/HomeAppBar.dart';
 import '../widgets/CategoriesWidget.dart';
 import '../widgets/ItemsWidget.dart';
@@ -7,91 +9,114 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: ListView(
-      children: [
-        HomeAppBar(),
-        Container(
-            //temp height
-            //height: 500,
-            padding: EdgeInsets.only(top: 15),
-            decoration: BoxDecoration(
-                color: Color(0xFFEDECF2),
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(35),
-                  topRight: Radius.circular(35),
-                )),
-            child: Column(
-              children: [
-                //Search Widget
-                Container(
-                    margin: EdgeInsets.symmetric(horizontal: 15),
-                    padding: EdgeInsets.symmetric(horizontal: 15),
-                    height: 50,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    child: Row(
-                      children: [
-                        Container(
-                          margin: EdgeInsets.only(left: 5),
-                          height: 50,
-                          //Changed witdth from 300 to 200
-                          width: 200,
-                          child: TextFormField(
-                              decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintText: "Search here...",
-                          )),
-                        ),
-                        Spacer(),
-                        Icon(
-                          Icons.camera_alt,
-                          size: 27,
-                          color: Color(0xFF4C53A5),
-                        )
-                      ],
-                    )),
-
-                //Categories
-                Container(
-                  alignment: Alignment.centerLeft,
-                  margin: EdgeInsets.symmetric(
-                    vertical: 20,
-                    horizontal: 10,
-                  ),
-                  child: Text("Categories",
-                      style: TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xFF4C53A5),
+      body: ListView(
+        children: [
+          HomeAppBar(),
+          Container(
+              //temp height
+              //height: 500,
+              padding: EdgeInsets.only(top: 15),
+              decoration: BoxDecoration(
+                  color: Color(0xFFEDECF2),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(35),
+                    topRight: Radius.circular(35),
+                  )),
+              child: Column(
+                children: [
+                  //Search Widget
+                  Container(
+                      margin: EdgeInsets.symmetric(horizontal: 15),
+                      padding: EdgeInsets.symmetric(horizontal: 15),
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: Row(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.only(left: 5),
+                            height: 50,
+                            //Changed witdth from 300 to 200
+                            width: 200,
+                            child: TextFormField(
+                                decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Search here...",
+                            )),
+                          ),
+                          Spacer(),
+                          Icon(
+                            Icons.camera_alt,
+                            size: 27,
+                            color: Color(0xFF4C53A5),
+                          )
+                        ],
                       )),
-                ),
 
-                // Categories Widget
+                  //Categories
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    margin: EdgeInsets.symmetric(
+                      vertical: 20,
+                      horizontal: 10,
+                    ),
+                    child: Text("Categories",
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF4C53A5),
+                        )),
+                  ),
 
-                CategoriesWidget(),
+                  // Categories Widget
 
-                //Items
+                  CategoriesWidget(),
 
-                Container(
-                  alignment: Alignment.centerLeft,
-                  margin: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
-                  child: Text(
-                    "Best Selling",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 25,
-                      color: Color(0xFF4C53A5),
+                  //Items
+
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    margin: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                    child: Text(
+                      "Best Selling",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25,
+                        color: Color(0xFF4C53A5),
+                      ),
                     ),
                   ),
-                ),
 
-                //Items Widget
-                ItemsWidget(),
-              ],
-            )),
-      ],
-    ));
+                  //Items Widget
+                  ItemsWidget(),
+                ],
+              )),
+        ],
+      ),
+      bottomNavigationBar: CurvedNavigationBar(
+          backgroundColor: Colors.transparent,
+          onTap: (index) {},
+          height: 70,
+          color: Color(0xFF4C53A5),
+          items: [
+            Icon(
+              Icons.home,
+              size: 30,
+              color: Colors.white,
+            ),
+            Icon(
+              CupertinoIcons.cart_fill,
+              size: 30,
+              color: Colors.white,
+            ),
+            Icon(
+              Icons.list,
+              size: 30,
+              color: Colors.white,
+            ),
+          ]),
+    );
   }
 }
