2a556e9b70688c9a670cd2827dc419708910d6d6
MARC_DIONIS_DUMANGAS <marcdionis.dumangas.20@usjr.edu.ph>
2023-02-08 10:16:19 +0000

    initial commit

---
+++ lib/pages/Homepage.dart
@@ -0,0 +1,15 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: ListView(
+      children: [
+        HomeAppBar(),
+      ],
+    ));
+  }
+}
