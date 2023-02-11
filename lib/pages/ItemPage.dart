bac1baba52bc6bb1c322b5af8dc1e93d063fe909
MARC_DIONIS_DUMANGAS <marcdionis.dumangas.20@usjr.edu.ph>
2023-02-11 09:48:23 +0000

    improved item page

--- lib/pages/ItemPage.dart
+++ lib/pages/ItemPage.dart
@@ -1,3 +1,4 @@
+import 'package:clippy_flutter/arc.dart';
 import 'package:flutter/material.dart';
 import '../widgets/ItemAppBar.dart';
 
@@ -10,6 +11,43 @@ class ItemPage extends StatelessWidget {
       body: ListView(
         children: [
           ItemAppBar(),
+          Padding(
+            padding: EdgeInsets.all(16),
+            child: Image.asset("images/1.png", height: 300),
+          ),
+          Arc(
+            edge: Edge.TOP,
+            arcType: ArcType.CONVEY,
+            height: 30,
+            child: Container(
+              width: 300,
+              height: 300,
+              color: Colors.white,
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 20,
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(
+                        top: 50,
+                        bottom: 20,
+                      ),
+                      child: Row(children: [
+                        Text(
+                          "Product Title",
+                          style: TextStyle(
+                            fontSize: 28,
+                          ),
+                        ),
+                      ]),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
