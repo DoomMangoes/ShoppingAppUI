0167d3350caf406748e30e6a52717aa3dc67ff4e
MARC_DIONIS_DUMANGAS <marcdionis.dumangas.20@usjr.edu.ph>
2023-02-11 11:02:47 +0000

    Finished itemPage. Added and finished ItemBottomNavBAr

--- lib/pages/ItemPage.dart
+++ lib/pages/ItemPage.dart
@@ -1,8 +1,18 @@
 import 'package:clippy_flutter/arc.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import '../widgets/ItemAppBar.dart';
+import '../widgets/ItemBottomNavBar.dart';
 
 class ItemPage extends StatelessWidget {
+  List<Color> Clrs = [
+    Colors.red,
+    Colors.green,
+    Colors.blue,
+    Colors.indigo,
+    Colors.orange,
+  ];
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -20,8 +30,7 @@ class ItemPage extends StatelessWidget {
             arcType: ArcType.CONVEY,
             height: 30,
             child: Container(
-              width: 300,
-              height: 300,
+              width: double.infinity,
               color: Colors.white,
               child: Padding(
                 padding: EdgeInsets.symmetric(
@@ -31,17 +40,191 @@ class ItemPage extends StatelessWidget {
                   children: [
                     Padding(
                       padding: EdgeInsets.only(
-                        top: 50,
-                        bottom: 20,
+                        top: 48,
+                        bottom: 15,
                       ),
                       child: Row(children: [
                         Text(
                           "Product Title",
                           style: TextStyle(
                             fontSize: 28,
+                            color: Color(0xFF4C53A5),
+                            fontWeight: FontWeight.bold,
                           ),
                         ),
                       ]),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 5, bottom: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          RatingBar.builder(
+                            initialRating: 4,
+                            minRating: 1,
+                            direction: Axis.horizontal,
+                            itemCount: 5,
+                            itemSize: 20,
+                            itemPadding: EdgeInsets.symmetric(horizontal: 4),
+                            itemBuilder: (context, _) => (Icon(
+                              Icons.favorite,
+                              color: Color(0xFF4C53A5),
+                            )),
+                            onRatingUpdate: (index) {},
+                          ),
+                          Row(
+                            children: [
+                              Container(
+                                padding: EdgeInsets.all(5),
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(20),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Colors.grey.withOpacity(0.5),
+                                        spreadRadius: 3,
+                                        blurRadius: 10,
+                                        offset: Offset(0, 3),
+                                      )
+                                    ]),
+                                child: Icon(
+                                  CupertinoIcons.minus,
+                                  size: 18,
+                                  color: Color(0xFF4C53A5),
+                                ),
+                              ),
+                              Container(
+                                  margin: EdgeInsets.all(5),
+                                  child: Text(
+                                    "01",
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                      color: Color(0xFF4C53A5),
+                                    ),
+                                  )),
+                              Container(
+                                padding: EdgeInsets.all(5),
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(20),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Colors.grey.withOpacity(0.5),
+                                        spreadRadius: 3,
+                                        blurRadius: 10,
+                                        offset: Offset(0, 3),
+                                      )
+                                    ]),
+                                child: Icon(
+                                  CupertinoIcons.plus,
+                                  size: 18,
+                                  color: Color(0xFF4C53A5),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 12),
+                      child: Text(
+                        "This is more detailed description of the product. You can write here more about the product. Never gonna give you up!",
+                        textAlign: TextAlign.justify,
+                        style: TextStyle(
+                          fontSize: 17,
+                          color: Color(0xFF4C53A5),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 8),
+                      child: Row(
+                        children: [
+                          Text(
+                            "Size:",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xFF4C53A5),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Row(
+                            children: [
+                              for (int i = 5; i < 10; i++)
+                                Container(
+                                  height: 30,
+                                  width: 30,
+                                  alignment: Alignment.center,
+                                  margin: EdgeInsets.symmetric(horizontal: 5),
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(30),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Colors.grey.withOpacity(0.5),
+                                        spreadRadius: 2,
+                                        blurRadius: 8,
+                                      ),
+                                    ],
+                                  ),
+                                  child: Text(
+                                    i.toString(),
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                      color: Color(0xFF4C53A5),
+                                    ),
+                                  ),
+                                ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 8),
+                      child: Row(
+                        children: [
+                          Text(
+                            "Color:",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xFF4C53A5),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Row(
+                            children: [
+                              for (int i = 0; i < 5; i++)
+                                Container(
+                                  height: 30,
+                                  width: 30,
+                                  alignment: Alignment.center,
+                                  margin: EdgeInsets.symmetric(horizontal: 5),
+                                  decoration: BoxDecoration(
+                                    color: Clrs[i],
+                                    borderRadius: BorderRadius.circular(30),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Colors.grey.withOpacity(0.5),
+                                        spreadRadius: 2,
+                                        blurRadius: 8,
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
                   ],
                 ),
@@ -50,6 +233,7 @@ class ItemPage extends StatelessWidget {
           ),
         ],
       ),
+      bottomNavigationBar: ItemBottomNavBar(),
     );
   }
 }
